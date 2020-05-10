f5081db41f1e948bc70a1fd52f650b2d5dc01387
Marko Budiselić <marko.budiselic@memgraph.com>
2020-05-10 22:06:41 +0200

    Add coverage script (#11)

---
+++ tool/coverage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+script_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)"
+project_dir="${script_dir}/.."
+
+# Cleanup all existing build files.
+rm -rf "${project_dir}/build" "${project_dir}/coverage" "${project_dir}/node_modules"
+
+# Generage CPP coverage report based on DEBUG build.
+cpp_cov_output_dir="${project_dir}/coverage/cpp-coverage"
+cpp_cov_target_file="${project_dir}/build/Debug/nodemgclient.node"
+mkdir -p "${cpp_cov_output_dir}"
+npm install --prefix "${project_dir}"
+npm run build:debug --prefix "${project_dir}"
+pushd "${project_dir}/build"
+cmake -DENABLE_COVERAGE=True .. && make
+popd
+LLVM_PROFILE_FILE="${cpp_cov_output_dir}/cpp.profraw" npm run test --prefix "${project_dir}"
+llvm-profdata merge -sparse "${cpp_cov_output_dir}/cpp.profraw" -o "${cpp_cov_output_dir}/cpp.profdata"
+llvm-cov show "${cpp_cov_target_file}" \
+    -format html -instr-profile "${cpp_cov_output_dir}/cpp.profdata" \
+    -o "${cpp_cov_output_dir}" \
+    -show-line-counts-or-regions -Xdemangler c++filt -Xdemangler -n
+
+# Generate JS coverage report based on RELEASE build.
+rm -rf "${project_dir}/build" "${project_dir}/node_modules"
+npm install --prefix "${project_dir}"
+npm run build:release --prefix "${project_dir}"
+npm run test:coverage --prefix "${project_dir}"
