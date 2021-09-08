5a7efebc9586b1abb15d59c2d2aed3e22f627a9b
Marko Budiselic <marko.budiselic@memgraph.com>
2021-09-08 17:59:45 +0200

    Add types

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+// Copyright (c) 2016-2021 Memgraph Ltd. [https://memgraph.com]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Keep in mind structural subtyping
+// https://www.typescriptlang.org/docs/handbook/type-compatibility.html
+// Keep in mind node-addon-api context
+// https://stackoverflow.com/questions/44275172/typescript-declarations-file-for-node-c-addon
+
+export interface Connection {
+  constructor(params?: any): Connection;
+  Execute(query: string, params?: any): Promise<any>;
+  DiscardAll(): Promise<any>;
+  FetchAll(): Promise<any>;
+  FetchOne(): Promise<any>;
+  Begin(): Promise<any>;
+  Commit(): Promise<any>;
+  Rollback(): Promise<any>;
+  ExecuteAndFetchAll(query: string, params?: any): Promise<any>;
+}
+
+export function Connect(params?: any): Promise<any>;
